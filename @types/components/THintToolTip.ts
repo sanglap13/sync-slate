fbabb8a3332baf569dfe35e27ead1aa34267acf9
arkajitroy <arkajitroy11.official@gmail.com>
2024-04-11 11:26:07 +0530

    Feat: add + configured HintToolTip component

---
+++ @types/components/THintToolTip.ts
@@ -0,0 +1,10 @@
+import React from "react";
+
+export type THintTooltipProps = {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+  alignOffset?: number;
+};
--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -4,15 +4,23 @@ import { CreateOrganization } from "@clerk/nextjs";
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { Plus } from "lucide-react";
+import HintTooltip from "@/components/shared/HintTooltip";
 
 const NewButton: React.FC = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
-          <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
-            <Plus className="text-white" />
-          </button>
+          <HintTooltip
+            label="Create Organization"
+            side="right"
+            align="start"
+            sideOffset={18}
+          >
+            <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+              <Plus className="text-white" />
+            </button>
+          </HintTooltip>
         </div>
       </DialogTrigger>
       <DialogContent className="p-0 bg-transparent border-none max-[400px]">
