b36c70a31e0621fe298613db0cb20605174ed99d
arkajitroy <arkajitroy11.official@gmail.com>
2024-04-11 10:24:18 +0530

    Feat: add organizationDialog component + functionalities

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import NewButton from "./new-button";
 
 const Sidebar: React.FC = (): JSX.Element => {
   return (
     <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
-      index
+      <NewButton />
     </aside>
   );
 };
