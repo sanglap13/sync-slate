fdafefdbda0732d8e954b2372aa76e68c2165647
sanglap13 <sanglapmridha@gmail.com>
2024-04-17 20:09:45 +0530

    fix: useDebounce issue in navbar

--- app/(dashboard)/_components/navbar/search-input.tsx
+++ app/(dashboard)/_components/navbar/search-input.tsx
@@ -3,19 +3,38 @@
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import { useDebounceCallback } from "usehooks-ts";
+import queryString from "query-string";
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { useDebounceValue } from "usehooks-ts";
 
 const SearchInput: React.FC = () => {
   const router = useRouter();
-  const [value, setValue] = useState("");
-  const debouncedValue = useDebounceCallback(value, 500);
+  const [searchValue, setSearchValue] = useState("");
+  const [debouncedValue, setValue] = useDebounceValue(searchValue, 500);
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
+
+  useEffect(() => {
+    const url = queryString.stringifyUrl(
+      {
+        url: "/",
+        query: { search: debouncedValue },
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+    router.push(url);
+  }, [debouncedValue, router]);
   return (
     <div className="w-full relative">
-      <Search className="absolute top-1/2 lef-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
+      <Search className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
       <Input
         className="w-full max-w-[516px] pl-9"
         placeholder="Search Slates"
+        onChange={handleSearchChange}
+        value={searchValue}
       />
     </div>
   );
