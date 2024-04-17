b216d2abde0147f4b8a8f0a9669ff0ad1fd648a4
sanglap13 <sanglapmridha@gmail.com>
2024-04-17 20:33:05 +0530

    feat: add conditional inviteMembers button in navbar

--- app/(dashboard)/_components/navbar/index.tsx
+++ app/(dashboard)/_components/navbar/index.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
-import { Search } from "lucide-react";
+import {
+  OrganizationSwitcher,
+  UserButton,
+  useOrganization,
+} from "@clerk/nextjs";
 import React from "react";
 import SearchInput from "./search-input";
+import InviteButton from "../inviteButton";
 
 const Navbar = () => {
+  const { organization } = useOrganization();
+
   return (
     <div className="flex items-center gap-x-4 p-5 ">
       <div className="hidden lg:flex lg:flex-1">
@@ -35,6 +41,7 @@ const Navbar = () => {
           }}
         />
       </div>
+      {organization && <InviteButton />}
       <UserButton />
     </div>
   );
