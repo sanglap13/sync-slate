1053f7f6a6e843ce88dcfae39abf73744a0b0f90
sanglap13 <sanglapmridha@gmail.com>
2024-04-19 21:52:20 +0530

    feat: add sonner message while making slate

--- app/(dashboard)/_components/boardList/empty-slates.tsx
+++ app/(dashboard)/_components/boardList/empty-slates.tsx
@@ -2,22 +2,29 @@
 
 import { Button } from "@/components/ui/button";
 import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 import { useOrganization } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
 import Image from "next/image";
 import React, { use } from "react";
+import { toast } from "sonner";
 
-const EmptySlates = () => {
+const EmptySlates: React.FC = () => {
   const { organization } = useOrganization();
-  const create = useMutation(api.board.create);
+  const { mutate, pending } = useApiMutation(api.board.create);
 
   const handleCreateClick = () => {
     if (!organization) return;
 
-    create({
+    mutate({
       orgId: organization.id,
       title: "Untitled Slate",
-    });
+    })
+      .then((id) => {
+        toast.success("Slate created successfully"); //TODO: redirect to board/id
+      })
+      .catch(() => {
+        toast.error("Failed to create slate");
+      });
   };
   return (
     <div className="h-full flex flex-col items-center justify-center">
@@ -32,7 +39,7 @@ const EmptySlates = () => {
         Start by creating a slate for your organization
       </p>
       <div className="mt-6">
-        <Button onClick={handleCreateClick} size={"lg"}>
+        <Button disabled={pending} onClick={handleCreateClick} size={"lg"}>
           Create Slate
         </Button>
       </div>
