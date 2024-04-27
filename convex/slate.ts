3426cb59b81c2379e47c5588e8f9ac14623e3daa
arkajitroy <arkajitroy11.official@gmail.com>
2024-04-27 13:16:52 +0530

    Feat: add delete + update functionality for the slates

--- convex/slate.ts
+++ convex/slate.ts
@@ -38,3 +38,35 @@ export const create = mutation({
     return slate;
   },
 });
+
+export const remove = mutation({
+  args: { id: v.id("boards") },
+  handler: async (context, args) => {
+    const identity = await context.auth.getUserIdentity();
+    if (!identity) throw new Error("Unauthorized!");
+
+    // Later check to delete favorite relation as well
+    await context.db.delete(args.id);
+  },
+});
+
+export const update = mutation({
+  args: { id: v.id("boards"), title: v.string() },
+  handler: async (context, args) => {
+    const title = args.title.trim();
+    const identity = args.title.trim();
+
+    // Error Handlings
+    if (!identity) throw new Error("Unauthorized!");
+    if (!title) throw new Error("Title is required!");
+    if (title.length > 60) {
+      throw new Error("Title cannot be longer than 60 characters");
+    }
+
+    const slate = await context.db.patch(args.id, {
+      title: args.title,
+    });
+
+    return slate;
+  },
+});
