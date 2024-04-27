987dc3e31bdf638e6def4e30278031645a464131
arkajitroy <arkajitroy11.official@gmail.com>
2024-04-27 20:12:14 +0530

    Refactor: changed the schema structure

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,8 +1,28 @@
 import { defineSchema, defineTable } from "convex/server";
 import { BoardsTable } from "./tables/boards";
 import { UserFavoriteTable } from "./tables/userFavorite";
+import { v } from "convex/values";
 
 export default defineSchema({
-  boards: BoardsTable,
-  userFavorites: UserFavoriteTable,
+  boards: defineTable({
+    title: v.string(),
+    orgId: v.string(),
+    authorId: v.string(),
+    authorName: v.string(),
+    imageUrl: v.string(),
+  })
+    .index("by_org", ["orgId"])
+    .searchIndex("search_title", {
+      searchField: "title",
+      filterFields: ["orgId"],
+    }),
+  userFavorites: defineTable({
+    orgId: v.string(),
+    userId: v.string(),
+    boardId: v.string(),
+  })
+    .index("by_board", ["boardId"])
+    .index("by_user_org", ["orgId"])
+    .index("by_user_board", ["userId", "boardId"])
+    .index("by_user_board_org", ["userId", "boardId", "orgId"]),
 });
