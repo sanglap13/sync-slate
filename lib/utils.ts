926e71925b14911d6c079d1fbc5da78b971dff4f
sanglap13 <sanglapmridha@gmail.com>
2024-04-30 17:26:05 +0530

    feat: add constant for participant borderColor

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = ["#d97706", "#7c3aed", "#059669", "#db2777", "#dc2626"];
+
+export function connectionIdToColor(connectionId: number): string {
+  return COLORS[connectionId % COLORS.length];
+}
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
